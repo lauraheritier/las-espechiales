0deebbe93ca2ca488bc6f66da631fd6b515ec72b
Laura Heritier <laura.heritier@gmail.com>
2022-03-29 12:10:44 -0300

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer className={styles.footer}>
       <div className={styles.prefooter}>
         <div><FaPhoneAlt /> 3834619507</div>
-        <a href="https://www.instagram.com/lasespechialescba/" target="_blank"><FaInstagram /> Instagram</a>
+        <a className={styles.instagram} href="https://www.instagram.com/lasespechialescba/" target="_blank"><FaInstagram /> Instagram</a>
         <a href="https://www.facebook.com/lasespechialescba" target="_blank"><FaFacebookSquare /> Facebook</a>
       </div>
       <div className={styles.copyright}>Copyright <span className={styles.brand}>Las Espechiales</span> &copy;{' '}
